3d279fb3b38a9d65a32d450ddb4f9d57f641dc77
Ewerthon Oliveira <ewerthonaraujo@outlook.com>
2024-01-29 13:42:15 -0300

    feat: pagina inicial com tabela dos produtos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { FormAddEditComponentComponent } from './form-add-edit-component/form-add-edit-component.component';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { FormAddEditComponentComponent } from './components/form-add-edit-component/form-add-edit-component.component';
+import { ProdutosService } from './services/produtos.service';
+import { CoreService } from './core/core.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'crud-app';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['codProduto', 'nomeProduto', 'precoProduto', 'descProduto', 'action'];
+  dataSource!: MatTableDataSource<Object>;
 
-  constructor(private _dialog: MatDialog){}
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  openAddEditEmpForm(){
-    this._dialog.open(FormAddEditComponentComponent)
+  constructor(
+    private _dialog: MatDialog,
+    private _productService: ProdutosService,
+    private _coreService: CoreService
+  ) { }
+
+  ngOnInit(): void {
+    this.getProductsList();
+  }
+
+  openAddEditEmpForm(): void {
+    const dialogRef = this._dialog.open(FormAddEditComponentComponent);
+
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getProductsList();
+        }
+      }
+    })
+  }
+
+  getProductsList(): void {
+    this._productService.getProdutosList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+
+        console.log(res)
+      },
+      error: (err) => {
+        console.error(err)
+      },
+    })
+  }
+
+  deleteProducts(idProduto: string): void {
+    this._productService.deleteProduto(idProduto).subscribe({
+      next: (res) => {
+        this.getProductsList();
+        this._coreService.openSnackBar('Produto deletado!')
+      },
+      error: (err) => {
+        console.error(err)
+      },
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  openEditForm(data: object): void {
+    console.log(typeof data)
+
+    const dialogRef = this._dialog.open(FormAddEditComponentComponent, {
+      data,
+    })
+
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getProductsList();
+        }
+      }
+    })
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatDialogModule} from '@angular/material/dialog';
-import { FormAddEditComponentComponent } from './form-add-edit-component/form-add-edit-component.component'
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { FormAddEditComponentComponent } from './components/form-add-edit-component/form-add-edit-component.component'
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,10 +30,13 @@ import { FormAddEditComponentComponent } from './form-add-edit-component/form-ad
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule, MatButtonModule, MatToolbarModule, MatDialogModule
+    MatIconModule, MatButtonModule, MatToolbarModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatTableModule, MatPaginatorModule, MatSortModule,
+    ReactiveFormsModule, MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }
   ],
   bootstrap: [AppComponent]
 })
